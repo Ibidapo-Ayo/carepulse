dcb3854a915e605003a03e7984dd93d3d44df8d0
Ibidapo Ayomide <ibidapoayomide754@gmail.com>
2024-07-18 16:03:41 -0700

    Disabled typescript

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,12 @@ const nextConfig = {
     images: {
         domains: ['cloud.appwrite.io'],
       },
+      typescript: {
+        ignoreBuildErrors: true
+      },
+      eslint: {
+        ignoreDuringBuilds: true
+      }
 };
 
 export default withSentryConfig(nextConfig, {
